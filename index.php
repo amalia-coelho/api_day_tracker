0fa0152caed30bee82bc31c6e497d8cd175f510f
Amália Coelho <apenas.coelho03@gmail.com>
2025-05-06 08:01:03 -0300

    Progresso na base do slim/slim

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  use Psr\Http\Message\ResponseInterface as Response;
+  use Psr\Http\Message\ServerRequestInterface as Request;
+  use Slim\Factory\AppFactory;
+
+  require __DIR__ . '/../vendor/autoload.php';
+
+  // Cria a instância da aplicação Slim
+  $app = AppFactory::create();
+
+  require __DIR__ . '/../src/Routes/route.dispatcher.php';
+
+  // Executa a aplicação
+  $app->run();
+
+?>
